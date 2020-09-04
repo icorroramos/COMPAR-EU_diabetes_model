63705e598a9737c41b03a599d415c6432a098555
gimondegraaf <gimondegraaf@gmail.com>
2020-09-04 16:23:51 +0200

    New Lola analyses

--- Lola_analyses.R
+++ Lola_analyses.R
@@ -1,5 +1,5 @@
 ### ANALYSES FOR LOLA MANUSCRIPT ###
-
+library(beepr)
 
 # IMPORT POST TRIAL PATIENT DATA ------------------------------------------
 
@@ -7,6 +7,7 @@ pop.intervention <- read.csv('input/baseline_characteristics_DESMOND_interventio
 
 pop.control <- read.csv('input/baseline_characteristics_DESMOND_control.csv')
 
+int.cost <- 76*1.25*1.1679
 
 # ADAPT INPUT DATA --------------------------------------------------------
 # Import CPI data (source: CBS, through FunCpi form ErasmuscorneR)
@@ -14,6 +15,10 @@ inflators <- read.csv('input/CPI_NL_2010_2019.csv')
 
 # Inflate Mt Hood costs inputs from 18 to 19
 cost_inputs <- cost_inputs * inflators[9, 2]
+
+# Inflate PAID estimates from 17 to 19
+future_medical_cost_inputs[, -1] <- future_medical_cost_inputs[, -1] * inflators[8, 2]
+future_nonmedical_cost_inputs[, -1] <- future_nonmedical_cost_inputs[, -1] * inflators[8, 2]
 
 # RUN SIMULATIONS ---------------------------------------------------------
 
@@ -34,11 +39,64 @@ sim.control.female <- SMDMII_model_simulation(1000, 1, 0, 0, 0, 0, 0.04, 0.015, 
 sim.finish <- Sys.time()
 
 print(sim.finish - sim.start)
-
+beep('mario')
 
 
 # PRESENT RESULTS ---------------------------------------------------------
+frac.male <- 0.54 # Percentage males in DESMOND
 
-res.tab.name <- c("Complication costs", "No complication costs", "Informal care costs", "Productivity costs", "Total costs", "Total QALYs")
+# Results tables
+res.tab.name <- c("Life expectancy", "Total QALYs", "Direct healthcare costs", "Informal care costs", "Productivity costs", "Future medical costs", "Future non-medical consumption", "Total costs")
 
-sim.int.male.tab <- data.frame(sim.int.male$mean_complication_costs)
+sim.int.male.tab <- data.frame(sim.int.male$mean_life_expectancy,
+                               sim.int.male$mean_total_qalys,
+                               sim.int.male$mean_complication_costs + sim.int.male$mean_nocomp_costs,
+                               sim.int.male$mean_inf_care_costs,
+                               sim.int.male$mean_prod_loss_costs, 
+                               sim.int.male$mean_future_medical_costs,
+                               sim.int.male$mean_future_nonmedical_costs,
+                               sim.int.male$mean_total_costs)
+
+names(sim.int.male.tab) <- res.tab.name
+
+sim.int.female.tab <- data.frame(sim.int.female$mean_life_expectancy,
+                               sim.int.female$mean_total_qalys,
+                               sim.int.female$mean_complication_costs + sim.int.female$mean_nocomp_costs,
+                               sim.int.female$mean_inf_care_costs,
+                               sim.int.female$mean_prod_loss_costs, 
+                               sim.int.female$mean_future_medical_costs,
+                               sim.int.female$mean_future_nonmedical_costs,
+                               sim.int.female$mean_total_costs)
+
+names(sim.int.female.tab) <- res.tab.name
+
+sim.control.male.tab <- data.frame(sim.control.male$mean_life_expectancy,
+                               sim.control.male$mean_total_qalys,
+                               sim.control.male$mean_complication_costs + sim.control.male$mean_nocomp_costs,
+                               sim.control.male$mean_inf_care_costs,
+                               sim.control.male$mean_prod_loss_costs, 
+                               sim.control.male$mean_future_medical_costs,
+                               sim.control.male$mean_future_nonmedical_costs,
+                               sim.control.male$mean_total_costs)
+
+names(sim.control.male.tab) <- res.tab.name
+
+sim.control.female.tab <- data.frame(sim.control.female$mean_life_expectancy,
+                                 sim.control.female$mean_total_qalys,
+                                 sim.control.female$mean_complication_costs + sim.control.female$mean_nocomp_costs,
+                                 sim.control.female$mean_inf_care_costs,
+                                 sim.control.female$mean_prod_loss_costs, 
+                                 sim.control.female$mean_future_medical_costs,
+                                 sim.control.female$mean_future_nonmedical_costs,
+                                 sim.control.female$mean_total_costs)
+
+names(sim.control.female.tab) <- res.tab.name
+
+# Calculate weighted average m/f ratio
+sim.int.weighed.tab <- (frac.male*sim.int.male.tab + (1 - frac.male) * sim.int.female.tab)
+
+sim.control.weighed.tab <- (frac.male*sim.control.male.tab + (1 - frac.male) * sim.control.female.tab)
+
+rbind(sim.control.weighed.tab, sim.int.weighed.tab )
+
+
