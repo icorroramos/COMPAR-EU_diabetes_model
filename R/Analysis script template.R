d26ab33390b8e9814fb68f8d8dfeca9799f2801e
iMTA <52324165+icorroramos@users.noreply.github.com>
2021-06-02 10:30:22 +0200

    Update Analysis script template.R

--- R/Analysis script template.R
+++ R/Analysis script template.R
@@ -1,8 +1,34 @@
-# Model function: 
-source("R/SMI in type II diabetes - HE model v3.R")
+##################
+# Set-up options #
+##################
+
+# Check and install the latest R version
+# installr::updateR()
+# update.packages() 
+
+# Remove all objects from the simulation
+rm(list = ls())
+
+# Setting option for decimals
+options(scipen = 3)
+
+# Install and load required packages
+# Sys.getenv() # check this for the Home path where packages are saved
+.libPaths(Sys.getenv()[["R_LIBS_USER"]])
+
+pkgs <- c("lattice", "MASS", "plyr", "survival", "tidyverse", "dplyr") # package names
+#install.packages(pkgs)
+inst <- lapply(pkgs, library, character.only = TRUE) # load them
 
 # Variable defined to keep track of simulation time (delete afterwards)
 init <- Sys.time()
+
+# Options below are divided into user adjustable and nonadjustable. The latter should NOT be changed by the user.
+
+# Model function: 
+source("R/SMI in type II diabetes - HE model v3.R")
+
+
 
 ############################
 # User adjustable settings #
