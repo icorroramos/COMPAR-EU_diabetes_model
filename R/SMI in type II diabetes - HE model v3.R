d6773a7035070c6c20fa564bf3428fb0345d1590
iMTA <52324165+icorroramos@users.noreply.github.com>
2021-06-14 13:24:28 +0200

    Update SMI in type II diabetes - HE model v3.R

--- R/SMI in type II diabetes - HE model v3.R
+++ R/SMI in type II diabetes - HE model v3.R
@@ -99,8 +99,8 @@ SMDMII_model_simulation <- function(patient_size_input, # numeric value > 0, pat
   
   # eGFR
   simulation_baseline_patients$eGFR       <- simulation_baseline_patients$eGFR/10 
-  simulation_baseline_patients$eGFR60more <- if_else(simulation_baseline_patients$eGFR >= 6, simulation_baseline_patients$eGFR-6, 0)
-  simulation_baseline_patients$eGFR60less <- if_else(simulation_baseline_patients$eGFR <  6, simulation_baseline_patients$eGFR, 6)
+  simulation_baseline_patients$eGFR60more <- if_else(simulation_baseline_patients$eGFR >= 6, simulation_baseline_patients$eGFR-6, 0) # confirmed
+  simulation_baseline_patients$eGFR60less <- if_else(simulation_baseline_patients$eGFR <  6, simulation_baseline_patients$eGFR, 6) # confirmed
   
   # HDL
   simulation_baseline_patients$HDL <- simulation_baseline_patients$HDL*10
@@ -110,7 +110,7 @@ SMDMII_model_simulation <- function(patient_size_input, # numeric value > 0, pat
   
   # LDL 
   simulation_baseline_patients$LDL       <- simulation_baseline_patients$LDL*10
-  simulation_baseline_patients$LDL35more <- if_else(simulation_baseline_patients$LDL >= 35, simulation_baseline_patients$LDL-35, 0)
+  simulation_baseline_patients$LDL35more <- if_else(simulation_baseline_patients$LDL >= 35, simulation_baseline_patients$LDL-35, 0) # confirmed
   
   # SBP
   simulation_baseline_patients$SBP <- simulation_baseline_patients$SBP/10
