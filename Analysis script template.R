0efc39101e04d9080ffa7ca598b8f00428ae4607
iMTA <52324165+icorroramos@users.noreply.github.com>
2021-07-15 15:52:45 +0200

    Running scenarios

--- Analysis script template.R
+++ Analysis script template.R
@@ -47,14 +47,14 @@ n_psa_input <- 5
 npats_input   <- 1000  # Try 5000 for testing purposes
 
 # Please indicate the name of the treatment to be identified
-tx_label <- "SBP_test"
+tx_label <- "female_hba1c_effect"
 
 # Treatment effect inputs
 treateff_start   <- 1 # Cycle in which treatment effect starts
 treateff_end     <- 4 # Cycle in which treatment effect ends
 treateff_decline <- 2 # Cycle in which treatment effect starts to decline (linearly)
 
-treateff_hba1c <- -0      #-0.8232 # Treatment effect on HbA1c (in absolute %-points HbA1c - baseline value HbA1c = 8.8)
+treateff_hba1c <- -0.8232      #-0.8232 # Treatment effect on HbA1c (in absolute %-points HbA1c - baseline value HbA1c = 8.8)
 treateff_hdl   <-  0*10   # Treatment effect on HDL-cholesterol (absolute effect, which unit?? - baseline value HDL = 3.08)-> TRANSFORMED IN CODE *10
 treateff_ldl   <- -0*10   # Treatment effect on LDL-cholesterol (absolute effect, which unit?? - baseline value LDL = 1.19)-> TRANSFORMED IN CODE *10
 treateff_bmi   <- -0      # Treatment effect on BMI (absolute effect in BMI units - baseline value BMI = 32.3)
