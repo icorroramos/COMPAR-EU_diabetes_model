42f0dab9ff0e89b27b0391ad08b134bfa967e941
iMTA <52324165+icorroramos@users.noreply.github.com>
2021-11-16 08:40:37 +0100

    Update TEST_analysis.R

--- R/TEST_analysis.R
+++ R/TEST_analysis.R
@@ -19,10 +19,10 @@
 # inst <- lapply(pkgs, library, character.only = TRUE) # load them
 
 # Load model function: 
-source("R/SMI in type II diabetes - HE model v3.R")
+source("SMI in type II diabetes - HE model v3.R")
 
 # Load aux. functions, input parameters (from Excel), global lists, etc. 
-source("R/aux_functions.R")
+source("aux_functions.R")
 
 
 # ANALYSIS INPUT VARIABLES ------------------------------------------------
@@ -62,14 +62,31 @@ treateff_start   <- 1 # Cycle in which treatment effect starts
 treateff_end     <- 3 # Cycle in which treatment effect ends
 treateff_decline <- 2 # Cycle in which treatment effect starts to decline (linearly)
 
+treateff_hba1c <- 0
+treateff_hdl   <- 0 
+treateff_ldl   <- 0 
+treateff_bmi   <- 0
+treateff_sbp   <- 0
 
 # Tx effects are vectors: the current assumption is that the same start, end and decline is assumed for all effect modifiers
 treatment_effect_HbA1c_input <- c(treateff_hba1c, treateff_start, treateff_end, treateff_decline)
-treatment_effect_HDL_input <- c(treateff_hdl, treateff_start, treateff_end, treateff_decline)
-treatment_effect_LDL_input <- c(treateff_ldl, treateff_start, treateff_end, treateff_decline)
-treatment_effect_BMI_input <- c(treateff_bmi, treateff_start, treateff_end, treateff_decline)
-treatment_effect_SBP_input <- c(treateff_sbp, treateff_start, treateff_end, treateff_decline)
+treatment_effect_HDL_input   <- c(treateff_hdl, treateff_start, treateff_end, treateff_decline)
+treatment_effect_LDL_input   <- c(treateff_ldl, treateff_start, treateff_end, treateff_decline)
+treatment_effect_BMI_input   <- c(treateff_bmi, treateff_start, treateff_end, treateff_decline)
+treatment_effect_SBP_input   <- c(treateff_sbp, treateff_start, treateff_end, treateff_decline)
 
+
+# Country-specific parameters
+inf_care_hours_input      <- 1.25318 # 15/11/2021: country-specific median = 1.25318 for UK, NL, DE and median = 2.26803 for ES, GR
+worked_hours_input        <- c(28, 38) # 15//11/21: VECTOR(female, male) = (28, 38) for UK, NL, DE and (30, 37.5) for ES, GR
+working_days_lost_input   <- c(14, 27.5) # 15/11/21: VECTOR(diabetes, diabetes & CHF/stroke) = (14, 27.5) for UK, NL, DE and (10, 20) for ES, GR
+cost_hour_sick_input      <- 25.781 # 15/11/21: UK=25.781 GBP, NL = 36.8 EUR, DE = 36.6 EUR, ES = 22.8 EUR, GR = 16.9 EUR
+friction_period_input     <- 82.18125 # 15/11/21: UK = 82.18125 days, NL= 85 days, DE = 69 days, ES = 75 days, GR = 98.6175 days
+informal_care_coef_input  <- "informal_care_coef_UK_NL_DE" # 15/11/21: regression coefficients -->> two equations informal_care_coef_UK_NL_DE and informal_care_coef_ES_GR -->> see aux_functions
+employment_coef_input     <- "employment_coef_UK_NL_DE" # 15/11/21: regression coefficients -->> two equations employment_coef_UK_NL_DE and  employment_coef_ES_GR -->> see aux_functions
+prod_costs_coef_input     <- "prod_costs_coef_UK_NL_DE" # 15/11/2021: regression coefficients -->> two equations prod_costs_coef_UK_NL_DE and prod_costs_coef_ES_GR   -->> see aux_functions
+inf_care_age_scale_input  <-  c(72.5474088, 10.4626624) # 15/11/2021: VECTOR(mean, sd) = (72.5474088, 10.4626624) for UK, NL, DE and VECTOR(mean, sd) = (76.0286769, 9.6290315) for ES, GR
+prod_loss_age_scale_input <-  c(60.2737989, 60.2737989) # 15/11/2021: VECTOR(mean, sd) = (60.2737989, 60.2737989) for UK, NL, DE and VECTOR(mean, sd) = (62.5992071, 6.6265962) for ES, GR
 
 # NOTE: Input parameters (probabilities, costs and utilities) can be changed in the corresponding 
 # csv files included in the folder "input". These can be changed to run for example scenario analyses
@@ -102,8 +119,21 @@ sim.results.female <- SMDMII_model_simulation(npats_input,
                                               discount_cost_input, 
                                               discount_util_input, 
                                               retirement_age_input, 
+                                              inf_care_hours_input, 
+                                              worked_hours_input, 
+                                              working_days_lost_input, 
+                                              cost_hour_sick_input, 
+                                              friction_period_input, 
+                                              informal_care_coef_input, 
+                                              employment_coef_input, 
+                                              prod_costs_coef_input, 
+                                              inf_care_age_scale_input, 
+                                              prod_loss_age_scale_input, 
                                               psa_input,
                                               seed_input)
+
+
+
 
 sim.results.female.comp <- SMDMII_model_simulation(npats_input,
                                                    female_input = 1,
