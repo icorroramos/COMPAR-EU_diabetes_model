3954841831d931bcf88e99a261da476f448a9952
iMTA <52324165+icorroramos@users.noreply.github.com>
2021-11-26 11:24:07 +0100

    Update TEST_analysis.R

--- R/TEST_analysis.R
+++ R/TEST_analysis.R
@@ -6,7 +6,6 @@
 # installr::updateR()
 # update.packages() 
 
-
 # Setting option for decimals
 options(scipen = 3)
 
@@ -40,7 +39,6 @@ retirement_age_input <- 66
 # Set random seed for replication purposes
 seed_input <- 958 # A random seed that it is used to ensure consistency in the model results.
 
-
 # LOAD MODEL FUNCTION AND AUX FUNCTIONS/ DATA -----------------------------
 
 # Load model function: 
@@ -49,7 +47,6 @@ source("R/SMI in type II diabetes - HE model v3.R")
 
 # Load aux. functions, input parameters (from Excel), global lists, etc. 
 source("R/aux_functions.R")
-
 
 init <- Sys.time()
 
@@ -63,11 +60,10 @@ treateff_decline <- 2 # Cycle in which treatment effect starts to decline (linea
 
 # Treatment effects
 treateff_hba1c <- -0.8232 # Treatment effect on HbA1c (in absolute %-points HbA1c)
-treateff_hdl <- 0 * 0.02586 * 10 # TRANSFORMATION *10 FOR MODEL INPUT # Treatment effect on HDL-cholesterol (absolute effect in mmol/l)
-treateff_ldl <- 0 * 0.02586 * 10 # TRANSFORMATION *10 FOR MODEL INPUT# Treatment effect on LDL-cholesterol (absolute effect in mmol/l)
-treateff_bmi <- -1.7 # Treatment effect on BMI (in absolute points)
-treateff_sbp <- -10.03 / 10 # TRANSFORMATION /10 FOR MODEL INPUT# Treatment effect on SBP (in absolute mmHg)
-
+treateff_hdl   <-  0 * 0.02586 * 10 # TRANSFORMATION *10 FOR MODEL INPUT # Treatment effect on HDL-cholesterol (absolute effect in mmol/l)
+treateff_ldl   <-  0 * 0.02586 * 10 # TRANSFORMATION *10 FOR MODEL INPUT# Treatment effect on LDL-cholesterol (absolute effect in mmol/l)
+treateff_bmi   <- -1.7 # Treatment effect on BMI (in absolute points)
+treateff_sbp   <- -10.03 / 10 # TRANSFORMATION /10 FOR MODEL INPUT# Treatment effect on SBP (in absolute mmHg)
 
 # Tx effects are vectors: the current assumption is that the same start, end and decline is assumed for all effect modifiers
 treatment_effect_HbA1c_input <- c(treateff_hba1c, treateff_start, treateff_end, treateff_decline)
@@ -75,7 +71,6 @@ treatment_effect_HDL_input   <- c(treateff_hdl, treateff_start, treateff_end, tr
 treatment_effect_LDL_input   <- c(treateff_ldl, treateff_start, treateff_end, treateff_decline)
 treatment_effect_BMI_input   <- c(treateff_bmi, treateff_start, treateff_end, treateff_decline)
 treatment_effect_SBP_input   <- c(treateff_sbp, treateff_start, treateff_end, treateff_decline)
-
 
 # NOTE: Input parameters (probabilities, costs and utilities) can be changed in the corresponding 
 # csv files included in the folder "input". These can be changed to run for example scenario analyses
@@ -91,9 +86,6 @@ treatment_effect_SBP_input   <- c(treateff_sbp, treateff_start, treateff_end, tr
 # The sim.vars object collects all parameters that define the simulation into one object
 # Then, it is saved with the output of the simulation. This way, if we  have multiple output files, we always have the information 
 # on the relevant input parameters that were used to produce the output.
-
-
-
 sim.vars <- list(seed_input, npats_input, tx_cost_input, mget(apropos('treateff.')))
 
 # RESULT OUTPUT FOR OUTPUT SCRIPT -----------------------------------------
@@ -121,7 +113,6 @@ sim.results.female <- SMDMII_model_simulation(patient_size_input = npats_input,
                                               run_PSA_input = psa_input,
                                               seed_input = seed_input)
 
-
 sim.results.female.comp <- SMDMII_model_simulation(patient_size_input = npats_input,
                                                    female_input = 1,
                                                    tx_cost_input = tx_cost_input,
@@ -145,7 +136,7 @@ sim.results.female.comp <- SMDMII_model_simulation(patient_size_input = npats_in
                                                    prod_loss_age_scale_input = prod_loss_age_scale_input,
                                                    run_PSA_input = psa_input,
                                                    seed_input = seed_input)
-  
+
 sim.results.male <- SMDMII_model_simulation(patient_size_input = npats_input,
                                             female_input = 0,
                                             tx_cost_input = tx_cost_input,
@@ -198,12 +189,10 @@ sim.results.male.comp <- SMDMII_model_simulation(patient_size_input = npats_inpu
 end <- Sys.time()
 print(end - init)
 
- 
 # Save simulation results
 save(sim.vars,
      sim.results.female,
      sim.results.female.comp,
      sim.results.male,
      sim.results.male.comp,
-     file = 'output/techver/TEST_RUN_1.RData') #file = paste0('output/', comp, '_seed_', seed_input, '.RData')
-
+     file = 'output/techver/TEST_RUN_Ref_scenario.RData') #file = paste0('output/', comp, '_seed_', seed_input, '.RData')
