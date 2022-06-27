1c6bacb3e8cec1f5935b31c0a91d1c406da85536
Gimon de Graaf <64062gdg@eur.nl>
2022-06-27 17:18:46 +0200

    Adapted to manually explore output for EuHEA scenario

--- R/Manual_output_CEA_of_SMI.R
+++ R/Manual_output_CEA_of_SMI.R
@@ -1,14 +1,22 @@
 # LOAD FUNCTIONS AND SET WD -----------------------------------------------
-wd.dir <- getwd()
+setwd('C:/Work/GitHub projects/COMPAR-EU_diabetes_model')
 
+# DASHBOARD - INPUT PARAMTERS ---------------------------------------------
+# Select country for output analysis based on identifier
+country.id <- 'UK'
+
+# Number of runs to compile for complete output
+n.runs <- 8
+
+# Choose which WTP threshold is used in the convergence plots
+wtp.conversion <- 50000
+
+# Cohortsize to calculate incidence on (i.e. incidence per x patients)
+inc.cohortsize <- 1000
+
+# LOAD FUNCTIONS AND SET WD -----------------------------------------------
 # Load stitcher function
-source(paste0(wd.dir, '/R/Stitcher.R'))
-
-
-## Override automatic working directory setting for manual selection of output to analyze
-setwd("C:/Work/GitHub projects/COMPAR-EU_diabetes_model")
-#setwd('\\\\mydocuments.eur.nl@SSL/DavWWWRoot/shared/groups/IMTA-600-H2020COMPAR-EU/600 Projectuitvoer/Disease models/Diabetes/Testing analysis settings')
-name.loc2 <- '5000_pats_seed-77/' # Name of subfolder with second run results (for use with stitcher function)
+source('R/Stitcher.R')
 
 # Fraction females in target pop
 frac.fem.allcountries <- data.frame(
@@ -20,9 +28,141 @@ frac.fem.allcountries <- data.frame(
 )
 frac.fem <- as.numeric(frac.fem.allcountries[country.id])
 
+
 wtp <- 20000
 
-# MANUAL ANALYSIS ---------------------------------------------------------
+
+# FUNCTIONS TO CALCULATE SCENARIOS ----------------------------------------
+
+calc.short.horizon <- function(sim.output, analysis.horizon){
+  full.data <- sim.output[[1]] %>% select(SIMID, SDURATION, discounted_TOTAL.COST, discounted_QALY)
+  within.horizon <- full.data %>% filter(SDURATION <= analysis.horizon)
+  patient.totals <- within.horizon %>% 
+    group_by(SIMID) %>% 
+    summarize(total_qalys = sum(discounted_QALY),
+              total_costs = sum(discounted_TOTAL.COST))
+  means <-patient.totals %>% summarise_all(mean) %>% select(-SIMID)
+}
+
+calc.additional.output <- function(sim.output) {
+  patient.cost.data <- sim.output[[1]] %>% select(SIMID, SDURATION, IHD.COST, MI.COST, CHF.COST, STROKE.COST, AMP.COST, BLIND.COST, ULCER.COST, RENAL.COST, NOCOMP.COST, INF.CARE.COST, PROD.LOSS.COST, FUTURE.COST.MEDICAL, FUTURE.COST.NONMEDICAL, cost_discount_factor)
+  discounted <- apply(patient.cost.data[, -c(1, 2, 16)], 2, '/', patient.cost.data[, 16])
+  patient.cost.data.discounted <- cbind(patient.cost.data[, 1:2], discounted)
+  patient.totals <- patient.cost.data.discounted %>% 
+    group_by(SIMID) %>% 
+    summarize(IHD_cost_disc = sum(IHD.COST),
+              MI_cost_disc = sum(MI.COST), 
+              CHF_cost_disc = sum(CHF.COST),
+              stroke_cost_disc = sum(STROKE.COST),
+              amput_cost_disc = sum(AMP.COST),
+              blind_cost_disc = sum(BLIND.COST),
+              ulcer_cost_disc = sum(ULCER.COST),
+              renal_cost_disc = sum(RENAL.COST),
+              uncomp_cost_disc = sum(NOCOMP.COST),
+              informal_care_cost_disc = sum(INF.CARE.COST),
+              production_losses_disc = sum(PROD.LOSS.COST),
+              future_medical_cost_disc = sum(FUTURE.COST.MEDICAL),
+              future_nonmedical_cost_disc = sum(FUTURE.COST.NONMEDICAL))
+  mean.costs <- patient.totals %>% summarise_all(mean) %>% select(-SIMID)
+  return(mean.costs)
+}
+
+calc.deltas.forD6.1 <- function(comp.name) {
+  # Grab outcomes that are delivered by simulation function
+  f.int.means <- as.data.frame(sim.results.female[-1])
+  f.uc.means <- as.data.frame(sim.results.female.comp[-1])
+  m.int.means <- as.data.frame(sim.results.male[-1])
+  m.uc.means <- as.data.frame(sim.results.male.comp[-1])
+  
+  w.int.means <- frac.fem * f.int.means + (1 - frac.fem) * m.int.means
+  w.uc.means <- frac.fem * f.uc.means + (1 - frac.fem) * m.uc.means
+  
+  delta.means <- w.int.means - w.uc.means
+  
+  # Get the additional output from the full patient histories via the calc.additional.output function
+  f.int.add.cost <- calc.additional.output(sim.results.female)
+  f.uc.add.cost <- calc.additional.output(sim.results.female.comp)
+  m.int.add.cost <- calc.additional.output(sim.results.male)
+  m.uc.add.cost <- calc.additional.output(sim.results.male.comp)
+  
+  w.int.add.means <- frac.fem * f.int.add.cost + (1 - frac.fem) * m.int.add.cost
+  w.uc.add.means <- frac.fem * f.uc.add.cost + (1 - frac.fem) * m.uc.add.cost
+  
+  delta.add.means <- w.int.add.means - w.uc.add.means
+  
+  all.means <- cbind(delta.means, delta.add.means)
+  
+  relevant.out <- all.means %>% 
+    transmute(comp = comp.name,
+              life_expecanty = round(mean_life_expectancy, 3),
+              incremental_total_qalys = round(mean_total_discounted_qalys, 3),
+              related_medical_costs = round(sum(IHD_cost_disc, MI_cost_disc, CHF_cost_disc, stroke_cost_disc, amput_cost_disc, blind_cost_disc, ulcer_cost_disc, renal_cost_disc, uncomp_cost_disc)),
+              unrelated_medical_costs = round(future_medical_cost_disc),
+              informal_care_costs = round(informal_care_cost_disc),
+              productivity_losses = round(production_losses_disc),
+              future_nonmedical_cons = round(future_nonmedical_cost_disc),
+              incremental_total_costs = round(mean_total_discounted_costs),
+              headroom_20k = round(20000 * mean_total_discounted_qalys - mean_total_discounted_costs),
+              headroom_50k = round(50000 * mean_total_discounted_qalys - mean_total_discounted_costs)
+    )
+}
+
+# CALCULATE SCNARIO OUTCOMES ----------------------------------------------
+
+# Results for scenario with extended treatment effect
+sewing_machine(n.runs, 'output/UK/Usual_care_outcomes_run_') # Usual care runs
+sewing_machine(n.runs, 'output/Scenarios/All_SMI_vs_UC_extended_effect_')
+output.extended <- calc.deltas.forD6.1('Any SMI extended effect')
+
+# Results for scenario with lifetime effect
+sewing_machine(n.runs, 'output/UK/Usual_care_outcomes_run_') # Usual care runs
+sewing_machine(n.runs, 'output/Scenarios/All_SMI_vs_UC_lifetime_effect_')
+output.lifetime <- calc.deltas.forD6.1('Any SMI lifetime effect')
+
+
+# Calculating short time horizons
+sewing_machine(n.runs, paste0('output/', country.id, '/Usual_care_outcomes_run_'))
+uc.run.seeds <- sewing.seeds
+sewing_machine(n.runs, paste0('output/', country.id, '/All_SMI_vs_UC_basecase_run_'))
+
+
+horizon.scenario <- 10
+
+f.int.sh <- calc.short.horizon(sim.results.female, horizon.scenario)
+f.uc.sh <- calc.short.horizon(sim.results.female.comp, horizon.scenario)
+m.int.sh <- calc.short.horizon(sim.results.male, horizon.scenario)
+m.uc.sh <- calc.short.horizon(sim.results.male.comp, horizon.scenario)
+
+w.int.means.sh <- frac.fem * f.int.sh + (1 - frac.fem) * m.int.sh
+w.uc.means.sh <- frac.fem * f.uc.sh + (1 - frac.fem) * m.uc.sh
+
+delta.means.sh <- w.int.means.sh - w.uc.means.sh
+
+
+sh.scenario.result <- delta.means.sh %>% 
+  transmute(incremental_total_qalys = round(total_qalys, 3),
+            incremental_total_costs = round(total_costs),
+            headroom_20k = round(20000 * total_qalys - total_costs),
+            headroom_50k = round(50000 * total_qalys - total_costs))
+
+print(sh.scenario.result)
+
+
+
+# SCENARIO FOR EUHEA PRESENTATION -----------------------------------------
+sewing_machine(n.runs, 'output/UK/Usual_care_outcomes_run_') # Usual care runs
+sewing_machine(n.runs, 'output/UK/LookAHEAD_run_')
+output.lookahead <- calc.deltas.forD6.1('LookAHEAD trial effect')
+
+# Extreme effect scenario
+sewing_machine(n.runs, 'output/UK/Usual_care_outcomes_run_') # Usual care runs
+sewing_machine(n.runs, 'output/UK/LookEXTREME_run_')
+output.extreme <- calc.deltas.forD6.1('Extreme LookAHED trial effect')
+
+
+# TEMP DUMP
+data.out <- rbind(output.lookahead, output.extreme)
+write.csv(data.out, 'Output_EUHEA_scenarios.csv', quote = FALSE)
 
 # LOAD SIMULATION DATA ----------------------------------------------------
 
@@ -96,28 +236,28 @@ print(compare.sex)
 
 # ANALYSIS OF INNER LOOP --------------------------------------------------
 # FOR EXTENDED DATASETS
-female.pp.uc <- sim.results.female.comp %>% 
+female.pp.uc <- sim.results.female.comp$simulation_patients_history %>% 
   group_by(SIMID) %>% 
   summarise(life_expectancy = max(SDURATION), 
             total_qaly = sum(QALY),
             total_cost = sum(TOTAL.COST)
   )
 
-male.pp.uc <- sim.results.male.comp %>% 
+male.pp.uc <- sim.results.male.comp$simulation_patients_history %>% 
   group_by(SIMID) %>% 
   summarise(life_expectancy = max(SDURATION), 
             total_qaly = sum(QALY),
             total_cost = sum(TOTAL.COST)
   )
 
-female.pp.int <- sim.results.female %>% 
+female.pp.int <- sim.results.female$simulation_patients_history %>% 
   group_by(SIMID) %>% 
   summarise(life_expectancy = max(SDURATION), 
             total_qaly = sum(QALY),
             total_cost = sum(TOTAL.COST)
   )
 
-male.pp.int <- sim.results.male %>% 
+male.pp.int <- sim.results.male$simulation_patients_history %>% 
   group_by(SIMID) %>% 
   summarise(life_expectancy = max(SDURATION), 
             total_qaly = sum(QALY),
@@ -182,7 +322,7 @@ for (i in 1:5) {
 
 
 
-
+# Old automated 
 # DETAILED ANALYSIS -------------------------------------------------------
 # diff.fem <- data.frame(
 #   Usual_care = t(f.uc.means),
